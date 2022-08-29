9cfc1d0d26a47c57e308deb39ec4e56791acad51
Hariharan542 <110983519+Hariharan542@users.noreply.github.com>
2022-08-29 18:10:26 +0530

    Create arith.ts

---
+++ arith.ts
@@ -0,0 +1,11 @@
+let a:Number=5;
+let b:Number=10;
+
+let add = Number(a)+Number(b);
+console.log('add:'+ add);
+let sub = Number(a)-Number(b);
+console.log('sub:', +sub);
+let mul = Number(a)*Number(b);
+console.log('mul :'+mul);
+let div =Number(a)/Number(b);
+console.log('div :'+div);
